e55739957839385b17cc0ebedc0f3da77b8ad1ad
kylecohen <kyle.cohen@outlook.com>
2021-07-13 15:57:19 -0700

    Replace original project with service project

---
+++ IPMonitor/IPMonitor.Service/Service1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPMonitor.Service
+{
+    public partial class IPMonitor : ServiceBase
+    {
+        public IPMonitor()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
